7316df73a13009b354b94c5be2f8e5fa325f665a
Vatih 2675 <ruslansamuel11@gmail.com>
2025-11-05 01:02:02 +0800

    Add Payment Channel

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ import logo from "/logo.png";
 import instagramIcon from "/instagram-icon.png";
 import facebookIcon from "/facebook-icon.svg";
 import tiktokIcon from "/tiktok-icon.png";
+import alloBankLogo from "/pembayaran/AlloBank.png";
+import bcaLogo from "/pembayaran/BCA.svg";
+import danaLogo from "/pembayaran/Dana.svg";
+import gopayLogo from "/pembayaran/GoPay.svg";
+import muamalatLogo from "/pembayaran/Muamalat.jpg";
+import ovoLogo from "/pembayaran/OVO.svg";
+import seaBankLogo from "/pembayaran/SeaBank.svg";
+import shopeePayLogo from "/pembayaran/ShopeePay.svg";
 
 function App() {
   const produk = "https://olshopin.com/t/hanamiprinting";
@@ -21,7 +29,7 @@ function App() {
       <img
         src={icon3}
         alt="Hanami Printing"
-        className="w-50 rounded-xl shadow-xl"
+        className="w-35 rounded-xl shadow-xl"
       />
       <div className="flex flex-col justify-center items-center cursor-default">
         <h1 className="font-bold m-0 text-orange-500 text-4xl">
@@ -49,7 +57,7 @@ function App() {
           title="Lokasi"
         ></a>
       </div>
-      <div className="flex flex-col justify-center items-center cursor-default">
+      <div className="w-full flex flex-col justify-center items-center cursor-default">
         <h1 className="font-bold mb-1 text-orange-500">Ikuti Toko Kami :</h1>
         <div className="mx-auto flex justify-center items-center gap-3">
           <a
@@ -78,6 +86,61 @@ function App() {
           </a>
         </div>
       </div>
+      <div className="w-full flex flex-col justify-center items-center cursor-default">
+        <h1 className="font-bold mb-1 text-orange-500">Channel Pembayaran :</h1>
+        <div className="mx-auto flex flex-wrap justify-center items-center gap-1 cursor-pointer">
+          <div
+            className="h-8 bg-white shadow-md rounded-full p-2 transition-all duration-300 hover:scale-120"
+            title="Dana - 0895708460123"
+          >
+            <img src={danaLogo} alt="Dana" className="h-full" />
+          </div>
+          <div
+            className="h-8 bg-white shadow-md rounded-full p-2 transition-all duration-300 hover:scale-120"
+            title="OVO - 0895708460123"
+          >
+            <img src={ovoLogo} alt="OVO" className="h-full" />
+          </div>
+          <div
+            className="h-8 bg-white shadow-md rounded-full p-2 transition-all duration-300 hover:scale-120"
+            title="Gopay - 0895708460123"
+          >
+            <img src={gopayLogo} alt="Gopay" className="h-full" />
+          </div>
+          <div
+            className="h-8 bg-white shadow-md rounded-full p-2 transition-all duration-300 hover:scale-120"
+            title="ShopeePay - 0895708460123"
+          >
+            <img src={shopeePayLogo} alt="ShopeePay" className="h-full" />
+          </div>
+          <div
+            className="h-8 bg-white shadow-md rounded-full p-2 transition-all duration-300 hover:scale-120"
+            title="BCA - 7975625991"
+          >
+            <img src={bcaLogo} alt="BCA" className="h-full" />
+          </div>
+          <div
+            className="h-8 bg-white shadow-md rounded-full p-2 transition-all duration-300 hover:scale-120"
+            title="Muamalat - 8110077620"
+          >
+            <img src={muamalatLogo} alt="Muamalat" className="h-full" />
+          </div>
+          <div
+            className="h-8 bg-white shadow-md rounded-full p-2 transition-all duration-300 hover:scale-120"
+            title="Sea Bank - 901849225983"
+          >
+            <img src={seaBankLogo} alt="Sea Bank" className="h-full" />
+          </div>
+          <div
+            className="h-8 bg-white shadow-md rounded-full p-2 transition-all duration-300 hover:scale-120"
+            title="Allo Bank - 0895708460123"
+          >
+            <img src={alloBankLogo} alt="Allo Bank" className="h-full" />
+          </div>
+        </div>
+      </div>
+      {/* <div className="flex justify-between items-start w-full">
+      </div> */}
     </section>
   );
 }
