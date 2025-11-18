a20607752443b0258c004658922e5d90d90fa9e2
vatih2675 <ruslansamuel11@gmail.com>
2025-11-18 14:34:08 +0800

    Edit viewport of small device (Services)

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,34 +3,47 @@ import ServicesBest from "../components/ServicesBest";
 
 export default function Services({dataLayanan}) {
     return (
-        <section className="w-full bg-orange-100">
-            <div className="w-full p-20 flex justify-center items-start cursor-default">
-                <div className="w-4/12 pe-10">
-                    <ServicesBest />
-                </div>
-                <div className="w-full ps-10 border-l border-gray-400">
-                    <h1 className="text-5xl text-orange-500 mb-10">Layanan Percetakan</h1>
-                    <div className="w-full grid grid-cols-3 gap-4">
-                        {dataLayanan.map(({judul, link, gambar, bintang, keterangan, online}, index) => (
-                            <NavLink to={`/layanan/${link}`} key={index} className="w-full bg-white rounded-2xl p-5 cursor-pointer hover:scale-105 transition-all duration-300 hover:shadow-black/20 hover:shadow-xl">
-                                <img
-                                    src={`./layanan/${gambar}`}
-                                    alt={judul}
-                                    className="w-full rounded-xl shadow-black/20 shadow-md mb-2"
-                                />
-                                <h1 className="text-center font-bold">{judul}</h1>
-                                <div className="w-full flex justify-center items-center gap-1">
-                                    <i className="bi-star-fill text-lg text-orange-400"></i>
-                                    <i className="bi-star-fill text-lg text-orange-400"></i>
-                                    <i className="bi-star-fill text-lg text-orange-400"></i>
-                                    <i className="bi-star-fill text-lg text-orange-400"></i>
-                                    <i className="bi-star-fill text-lg text-orange-400"></i>
-                                </div>
-                            </NavLink>
-                        ))}
+      <section className="w-full bg-orange-100">
+        <div className="w-full p-3 sm:p-20 flex justify-center items-start cursor-default">
+          <div className="w-4/12 pe-10 hidden sm:inline-block">
+            <ServicesBest />
+          </div>
+          <div className="w-full sm:ps-10 sm:border-l border-gray-400">
+            <h1 className="text-center sm:text-start text-3xl sm:text-5xl text-orange-500 mb-5 sm:mb-10">
+              Layanan Hanami Printing
+            </h1>
+            <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-4">
+              {dataLayanan.map(
+                (
+                  { judul, link, gambar, bintang, keterangan, online },
+                  index
+                ) => (
+                  <NavLink
+                    to={`/layanan/${link}`}
+                    key={index}
+                    className="w-full bg-white rounded-lg sm:rounded-2xl p-3 sm:p-5 cursor-pointer hover:scale-105 transition-all duration-300 hover:shadow-black/20 hover:shadow-xl"
+                  >
+                    <img
+                      src={`./layanan/${gambar}`}
+                      alt={judul}
+                      className="w-full rounded-md sm:rounded-xl shadow-black/20 shadow-md mb-2"
+                    />
+                    <h1 className="text-center text-sm sm:text-base font-bold">
+                      {judul}
+                    </h1>
+                    <div className="w-full flex justify-center items-center gap-1">
+                      <i className="bi-star-fill text-xs sm:text-lg text-orange-400"></i>
+                      <i className="bi-star-fill text-xs sm:text-lg text-orange-400"></i>
+                      <i className="bi-star-fill text-xs sm:text-lg text-orange-400"></i>
+                      <i className="bi-star-fill text-xs sm:text-lg text-orange-400"></i>
+                      <i className="bi-star-fill text-xs sm:text-lg text-orange-400"></i>
                     </div>
-                </div>
+                  </NavLink>
+                )
+              )}
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
     );
 }
--- src/pages/services/CetakAgenda.jsx
+++ src/pages/services/CetakAgenda.jsx
@@ -3,41 +3,60 @@ import ServicesBest from "../../components/ServicesBest";
 
 export default function CetakAgenda({dataApp}) {
     return (
-        <section className="w-full bg-orange-100">
-            <div className="w-full p-20 flex justify-center items-start cursor-default">
-                <div className="w-4/12 pe-10 border-r border-gray-400">
-                    <ServicesBest />
-                </div>
-                <div className="w-full ps-10">
-                    <div className="flex justify-start items-center w-full gap-2 mb-10 text-3xl">
-                        <NavLink to="/layanan" className="text-3xl hover:text-orange-500 transition-all duration-300"><i className="bi-arrow-left me-2"></i>Layanan</NavLink>
-                        <span>/</span>
-                        <h1 className="text-3xl text-orange-500 font-bold">Cetak Agenda</h1>
-                    </div>
-                    <div className="w-full bg-white rounded-xl p-5 shadow-black/10 shadow-lg">
-                        <div className="w-full flex justify-between items-center mb-5">
-                            <h3 className="text-orange-500 font-bold text-xl">Cetak Agenda</h3>
-                            <div className="w-fit flex justify-center items-center gap-1">
-                                <i className="bi-star-fill text-lg text-orange-400"></i>
-                                <i className="bi-star-fill text-lg text-orange-400"></i>
-                                <i className="bi-star-fill text-lg text-orange-400"></i>
-                                <i className="bi-star-fill text-lg text-orange-400"></i>
-                                <i className="bi-star-fill text-lg text-orange-400"></i>
-                            </div>
-                        </div>
-                        <div className="relative flex justify-center items-start gap-5">
-                            <img src="../layanan/cetak-agenda.png" alt="" className="w-4/12 rounded-xl shadow-black/10 shadow-lg" />
-                            <div className="w-full">
-                                <p className="text-justify">Cetak Agenda untuk berbagai kegiatan Anda.</p>
-                            </div>
-                            <a href={dataApp.links.whatsapp} target="_blank" className="absolute w-fit flex justify-center items-center gap-2 rounded-lg bg-orange-600 px-3 py-2 text-white text-sm shadow-black/20 shadow-md hover:bg-orange-500 transition-all duration-300 bottom-0 right-0">
-                                <i className="bi-whatsapp"></i>
-                                <span>Pesan Sekarang</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+      <section className="w-full bg-orange-100">
+        <div className="w-full p-3 sm:p-20 flex justify-center items-start cursor-default">
+          <div className="w-4/12 hidden sm:inline-block pe-10 border-r border-gray-400">
+            <ServicesBest />
+          </div>
+          <div className="w-full sm:ps-10">
+            <div className="flex justify-start items-center w-full gap-2 mb-5 sm:mb-10 text-xl sm:text-3xl">
+              <NavLink
+                to="/layanan"
+                className="text-xl sm:text-3xl hover:text-orange-500 transition-all duration-300"
+              >
+                <i className="bi-arrow-left me-2"></i>Layanan
+              </NavLink>
+              <span>/</span>
+              <h1 className="text-xl sm:text-3xl text-orange-500 font-bold">
+                Cetak Agenda
+              </h1>
             </div>
-        </section>
-    )
+            <div className="w-full bg-white rounded-xl p-3 sm:p-5 shadow-black/10 shadow-lg mb-5">
+              <div className="w-full flex justify-between items-center mb-3 sm:mb-5">
+                <h3 className="text-orange-500 font-bold sm:text-xl">
+                  Cetak Agenda
+                </h3>
+                <div className="w-fit flex text-xs sm:text-lg justify-center items-center gap-1">
+                  <i className="bi-star-fill text-orange-400"></i>
+                  <i className="bi-star-fill text-orange-400"></i>
+                  <i className="bi-star-fill text-orange-400"></i>
+                  <i className="bi-star-fill text-orange-400"></i>
+                  <i className="bi-star-fill text-orange-400"></i>
+                </div>
+              </div>
+              <div className="relative flex justify-center items-start gap-3 sm:gap-5">
+                <img
+                  src="../layanan/cetak-agenda.png"
+                  alt=""
+                  className="w-3/12 sm:w-4/12 rounded-xl shadow-black/10 shadow-lg"
+                />
+                <div className="w-full">
+                  <p className="text-xs sm:text-base text-justify">
+                    Cetak Agenda untuk berbagai kegiatan Anda.
+                  </p>
+                </div>
+                <a
+                  href={dataApp.links.whatsapp}
+                  target="_blank"
+                  className="absolute w-fit flex justify-center items-center gap-2 rounded-lg bg-orange-600 px-2 sm:px-3 py-1 sm:py-2 text-white text-xs sm:text-sm shadow-black/20 shadow-md hover:bg-orange-500 transition-all duration-300 bottom-0 right-0"
+                >
+                  <i className="bi-whatsapp"></i>
+                  <span>Pesan Sekarang</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
 }
